69b8979e68cde58a2454d8bd7b49746e765c28a4
Mickyappz <30460821+Mickyappz@users.noreply.github.com>
2017-09-01 12:59:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    /*var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
-        speech: speech,
+        speech: ' HEllo Roney How are you',
         displayText: speech,
         source: 'webhook-echo-sample'
-    });*/
-	var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-	var play = require('play');   
-   return res.json(play.sound('http://d97smbw2u73ny.cloudfront.net/master.m3u8'));
+    }); 
+	 
 	 
 });
 
